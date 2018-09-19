767d27561f6e74eb072c40ad92867c73a1280512
kameshGithub <kamesh.chauhan@gmail.com>
2018-09-19 13:48:43 +0530

    Revert "initial commit"
    This reverts commit 19d85ec0266c5ec1b8a9949fcf188fff2639d68c.

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -8,8 +8,7 @@ import { Employee } from './employee';
 export class EmployeeService {
 
   private employee:Employee=null;
-  //private baseUrl = 'https://empmgmt.herokuapp.com/api/';
-  private baseUrl = 'http://localhost:5000/api/';
+  private baseUrl = 'https://empmgmt.herokuapp.com/api/';
   private EMPLOYEES = 'employees';
   private EMPLOYEE = 'employee';  // used for create employee only
 
